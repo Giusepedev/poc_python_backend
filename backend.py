86e885ef100d27e9bcaf7b5329b0fe572ee74f05
Giusepe <giudevops@gmail.com>
2021-01-16 21:25:39 -0300

    adicionando password como variavel de ambiente

--- backend.py
+++ backend.py
@@ -1,16 +1,17 @@
 import mysql.connector
+import os
 
 mydb = mysql.connector.connect(
     host = "127.0.0.1",
     port = "3306",
     user = "root",
-    password = "",
+    password = os.environ['password'],
     database = "sql_hr"
 )
 
 mycursor = mydb.cursor()
 
-sql = "UPDATE employees SET first_name = 'jesk' WHERE employee_id = '33391'"
+sql = "UPDATE employees SET first_name = 'julia' WHERE employee_id = '33391'"
 
 mycursor.execute(sql)
 
